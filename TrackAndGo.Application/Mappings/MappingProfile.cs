84547715435190d4f61b52083fa21fc2a7860ee0
Daniel Moisei <daniel.moisei@isa.utm.md>
2025-04-11 16:21:49 +0300

    Added hotel command

---
+++ TrackAndGo.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TrackAndGo.Application.DTOs;
+using TrackAndGo.Domain.Entities;
+
+namespace TrackAndGo.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Hotel, HotelDto>().ReverseMap();
+        }
+    }
+}
