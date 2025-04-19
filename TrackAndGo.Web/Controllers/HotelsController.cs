9c709d3317df29d63223e6384c3eba2b80506c70
Gabriela <gabrielapr2140@gmail.com>
2025-04-19 09:24:34 +0300

    Added get hotels endpoint

---
+++ TrackAndGo.Web/Controllers/HotelsController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TrackAndGo.Application.Queries;
+
+namespace TrackAndGo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public HotelsController(IMediator mediator) 
+        { 
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHotels()
+        {
+            var query = new GetHotelsQuery();
+
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+    }
+}
